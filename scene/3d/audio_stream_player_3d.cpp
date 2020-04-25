6917a14923ecccfb74e446cb11e921b042abc709
theludovyc <theludovyc@gmail.com>
2020-04-25 20:08:44 +0200

    scene\3d\audio_stream_player_3d.cpp

--- scene/3d/audio_stream_player_3d.cpp
+++ scene/3d/audio_stream_player_3d.cpp
@@ -46,12 +46,12 @@ private:
 		mutable real_t squared_gain; // temporary
 	};
 
-	Vector<Speaker> speakers;
+	std::vector<Speaker> speakers;
 
 public:
 	Spcap(unsigned int speaker_count, const Vector3 *speaker_directions) {
 		this->speakers.resize(speaker_count);
-		Speaker *w = this->speakers.ptrw();
+		Speaker *w = this->speakers.data();
 		for (unsigned int speaker_num = 0; speaker_num < speaker_count; speaker_num++) {
 			w[speaker_num].direction = speaker_directions[speaker_num];
 			w[speaker_num].squared_gain = 0.0;
@@ -67,11 +67,11 @@ public:
 	}
 
 	Vector3 get_speaker_direction(unsigned int index) const {
-		return this->speakers.ptr()[index].direction;
+		return this->speakers[index].direction;
 	}
 
 	void calculate(const Vector3 &source_direction, real_t tightness, unsigned int volume_count, real_t *volumes) const {
-		const Speaker *r = this->speakers.ptr();
+		const Speaker *r = this->speakers.data();
 		real_t sum_squared_gains = 0.0;
 		for (unsigned int speaker_num = 0; speaker_num < (unsigned int)this->speakers.size(); speaker_num++) {
 			real_t initial_gain = 0.5 * powf(1.0 + r[speaker_num].direction.dot(source_direction), tightness) / r[speaker_num].effective_number_of_speakers;
