66ba2c6e0d075effca6046962c3720735526210a
Flavien PERINEAU <feyleth@gmail.com>
2020-05-18 19:25:04 +0200

    add windows support for cmake

---
+++ platform/windows/definition.cmake
@@ -0,0 +1,13 @@
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+    add_definitions(-DDEBUG_ENABLED)
+    add_definitions(-DDEBUG_MEMORY_ENABLED)
+    add_definitions(-DD3D_DEBUG_INFO)
+endif()
+
+
+add_definitions(-DWINDOWS_ENABLED -DOPENGL_ENABLED -DWASAPI_ENABLED -DWINMIDI_ENABLED)
+
+add_definitions(-DWINVER=0x0601 -D_WIN32_WINNT=0x0601)
+
+add_definitions(-DMINGW_ENABLED)
+add_definitions(-DMINGW_HAS_SECURE_API=1)
