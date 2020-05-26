e4a710ec619df8fe1f5ec99b3b1f871d7f6a72d9
Flavien PERINEAU <feyleth@gmail.com>
2020-05-26 20:41:45 +0200

    correct build for ninja

--- cmake/generate.cmake
+++ cmake/generate.cmake
@@ -20,16 +20,3 @@ add_custom_command(OUTPUT "${dir}/logo.gen.h" ${iconFile}
      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/script"
 )
 endmacro()
-
-
-# TODO : temporary
-macro(generateBindings gen)
-
-add_custom_command(OUTPUT "${CMAKE_SOURCE_DIR}/core/method_bind.gen.inc" "${CMAKE_SOURCE_DIR}/core/method_bind_ext.gen.inc" "${CMAKE_SOURCE_DIR}/core/method_bind_free_func.gen.inc"
-  COMMAND ${Python_EXECUTABLE} "${CMAKE_SOURCE_DIR}/script/generate_binders.py"
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/script"
-	)
-list(APPEND ${gen} "${CMAKE_SOURCE_DIR}/core/method_bind.gen.inc" "${CMAKE_SOURCE_DIR}/core/method_bind_ext.gen.inc" "${CMAKE_SOURCE_DIR}/core/method_bind_free_func.gen.inc")
-
-endmacro()
-
