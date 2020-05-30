0b4536658066b1c3236804bbaebeaa313be40f7e
MrMartinIden <MrMartinIden@protonmail.com>
2020-05-30 17:50:07 +0300

    core/io/logger.cpp

--- core/io/logger.cpp
+++ core/io/logger.cpp
@@ -248,7 +248,7 @@ void CompositeLogger::logv(const char *p_format, va_list p_list, bool p_err) {
 		return;
 	}
 
-	for (int i = 0; i < loggers.size(); ++i) {
+	for (decltype(loggers.size()) i = 0; i < loggers.size(); ++i) {
 		va_list list_copy;
 		va_copy(list_copy, p_list);
 		loggers[i]->logv(p_format, list_copy, p_err);
@@ -261,7 +261,7 @@ void CompositeLogger::log_error(const char *p_function, const char *p_file, int 
 		return;
 	}
 
-	for (int i = 0; i < loggers.size(); ++i) {
+	for (decltype(loggers.size()) i = 0; i < loggers.size(); ++i) {
 		loggers[i]->log_error(p_function, p_file, p_line, p_code, p_rationale, p_type);
 	}
 }
@@ -271,7 +271,7 @@ void CompositeLogger::add_logger(Logger *p_logger) {
 }
 
 CompositeLogger::~CompositeLogger() {
-	for (int i = 0; i < loggers.size(); ++i) {
+	for (decltype(loggers.size()) i = 0; i < loggers.size(); ++i) {
 		memdelete(loggers[i]);
 	}
 }
