5a982d3b93a4e8d92b33e2f898bd6d1e95ebb969
lsviance <ludovic.de-saint-viance@u-bordeaux.fr>
2021-06-21 18:04:19 +0200

    core

--- core/core.cmake
+++ core/core.cmake
@@ -5,21 +5,6 @@ add_custom_command(
         DEPENDS pygen_script/generate_default_controller_mappings.py
 )
 
-file(GLOB src_core *.cpp)
+file(GLOB_RECURSE src_core core/*.cpp)
 
-file(GLOB_RECURSE
-        src_core1
-        core/config/*.cpp
-        core/debugger/*.cpp
-        core/error/*.cpp
-        core/input/*.cpp
-        core/io/*.cpp
-        core/math/*.cpp
-        core/object/*.cpp
-        core/os/*.cpp
-        core/string/*.cpp
-        core/templates/*cpp
-        core/variant/*.cpp
-        )
-
-list(APPEND SRC ${src_core} ${src_core1} core/input/default_controller_mappings.gen.cpp)
+list(APPEND SRC ${src_core} core/input/default_controller_mappings.gen.cpp)
