88dbbce75e60dd53ecead835a6d948a44bf5e6c3
lsviance <ludovic.de-saint-viance@u-bordeaux.fr>
2021-06-22 11:09:22 +0200

    embree macro

--- thirdparty/thirdparty.cmake
+++ thirdparty/thirdparty.cmake
@@ -1,17 +1,26 @@
 include(thirdparty/basis_universal/basis_universal.cmake)
 
-add_compile_definitions(BT_USE_OLD_DAMPING_METHOD)
-file(GLOB_RECURSE src_bullet thirdparty/bullet/*.c)
-list(APPEND SRC ${src_bullet})
+macro(buildThirdpartyFile dirName ext)
+    file(GLOB_RECURSE src_${dirName} thirdparty/${dirName}/${ext})
+    list(APPEND SRC ${src_${dirName}})
+endmacro()
 
-file(GLOB_RECURSE src_zstd thirdparty/zstd/*.c)
-list(APPEND SRC ${src_zstd})
+macro(buildThirdpartyFileC dirName)
+    buildThirdpartyFile(${dirName} *.c)
+endmacro()
+
+macro(buildThridparyFileCpp dirName)
+    buildThirdpartyFile(${dirName} *.cpp)
+endmacro()
+
+add_compile_definitions(BT_USE_OLD_DAMPING_METHOD)
+buildThirdpartyFileC(bullet)
+
+buildThirdpartyFileC(zstd)
 
 list(APPEND SRC thirdparty/cvtt/ConvectionKernels.cpp)
 
-#add_subdirectory(cvtt)
-
-#add_subdirectory(embree)
+#buildThridparyFileCpp(embree)
 
 #add_subdirectory(enet)
 
