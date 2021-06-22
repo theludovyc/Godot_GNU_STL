2b6f9093ef056605484692b842f5f941f01a374b
lsviance <ludovic.de-saint-viance@u-bordeaux.fr>
2021-06-22 15:42:09 +0200

    scene

---
+++ scene/scene.cmake
@@ -0,0 +1,11 @@
+add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/scene/resources/default_theme/default_font.gen.h
+        COMMAND ${Python3_EXECUTABLE} pygen_script/generate_default_font.py
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        DEPENDS pygen_script/generate_default_font.py thirdparty/fonts/OpenSans_SemiBold.ttf
+)
+
+list(APPEND SRC ${CMAKE_CURRENT_SOURCE_DIR}/scene/resources/default_theme/default_font.gen.h)
+
+file(GLOB_RECURSE src_scene scene/*.cpp)
+list(APPEND SRC ${src_scene})
