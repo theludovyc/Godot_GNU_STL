9f101bdff3be5873e8873728c1b854f81315d660
lsviance <ludovic.de-saint-viance@u-bordeaux.fr>
2021-06-22 10:57:43 +0200

    core macro bullet

--- core/core.cmake
+++ core/core.cmake
@@ -1,3 +1,29 @@
+macro(generateCoreFile fileName)
+    add_custom_command(
+            OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/core/${fileName}.gen.h
+            COMMAND ${Python3_EXECUTABLE} pygen_script/generate_${fileName}.py
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            DEPENDS pygen_script/generate_${fileName}.py
+    )
+
+    list(APPEND SRC ${CMAKE_CURRENT_SOURCE_DIR}/core/${fileName}.gen.h)
+endmacro()
+
+generateCoreFile(authors)
+
+generateCoreFile(donors)
+
+generateCoreFile(license)
+
+add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/core/version_generated.gen.h ${CMAKE_CURRENT_SOURCE_DIR}/core/version_hash.gen.h
+        COMMAND ${Python3_EXECUTABLE} pygen_script/generate_version.py
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        DEPENDS pygen_script/generate_version.py
+)
+
+list(APPEND SRC ${CMAKE_CURRENT_SOURCE_DIR}/core/version_generated.gen.h ${CMAKE_CURRENT_SOURCE_DIR}/core/version_hash.gen.h)
+
 add_custom_command(
         OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/core/input/default_controller_mappings.gen.cpp
         COMMAND ${Python3_EXECUTABLE} pygen_script/generate_default_controller_mappings.py
@@ -5,6 +31,8 @@ add_custom_command(
         DEPENDS pygen_script/generate_default_controller_mappings.py
 )
 
+list(APPEND SRC ${CMAKE_CURRENT_SOURCE_DIR}/core/input/default_controller_mappings.gen.cpp)
+
 file(GLOB_RECURSE src_core core/*.cpp)
 
-list(APPEND SRC ${src_core} core/input/default_controller_mappings.gen.cpp)
+list(APPEND SRC ${src_core})
--- thirdparty/thirdparty.cmake
+++ thirdparty/thirdparty.cmake
@@ -3,6 +3,10 @@ include(thirdparty/basis_universal/basis_universal.cmake)
 file(GLOB_RECURSE src_zstd thirdparty/zstd/*.c)
 list(APPEND SRC ${src_zstd})
 
+add_compile_definitions(BT_USE_OLD_DAMPING_METHOD)
+file(GLOB_RECURSE src_bullet thirdparty/bullet/*.c)
+list(APPEND SRC ${src_bullet})
+
 #add_subdirectory(bullet)
 
 #add_subdirectory(cvtt)
