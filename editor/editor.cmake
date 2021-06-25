a8f8934ddd30d373dab02c3549892b3b832ab0c7
theludovyc <theludovyc@gmail.com>
2021-06-25 11:25:26 +0200

    it's me !

--- editor/editor.cmake
+++ editor/editor.cmake
@@ -6,6 +6,8 @@ generateFile(editor/editor_icons)
 
 generateFile(editor/doc_translations)
 
+generateFile(editor/editor_translations)
+
 file(GLOB_RECURSE src_editor editor/*.cpp)
 
 list(APPEND SRC ${src_editor})
