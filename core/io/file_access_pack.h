9693cf19646b0b12c7896dffd105cb93a2ead654
theludovyc <theludovyc@gmail.com>
2021-07-01 15:23:41 +0200

    update

--- core/io/file_access_pack.h
+++ core/io/file_access_pack.h
@@ -93,7 +93,7 @@ private:
 
 		PathMD5() {}
 
-		PathMD5(const Vector<uint8_t> &p_buf) {
+		PathMD5(const std::vector<uint8_t> &p_buf) {
 			a = *((uint64_t *)&p_buf[0]);
 			b = *((uint64_t *)&p_buf[8]);
 		}
@@ -101,7 +101,7 @@ private:
 
 	Map<PathMD5, PackedFile> files;
 
-	Vector<PackSource *> sources;
+	std::vector<PackSource *> sources;
 
 	PackedDir *root;
 
